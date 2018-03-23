48a96d3220fd52946b47b32371a0bc2066b727a1
Artem Bolshakov <a.t.bolsh@gmail.com>
2018-03-23 18:12:02 -0400

    Lots of exploration. L2 and L3 converge on the same clique. L4 gets a bigger k. Attractors saved.

--- testingRepel.py
+++ testingRepel.py
@@ -1,4 +1,4 @@
-from NPmat import *
+from NPrepel import *
 from graphReader import getGraph
 from cliqueProject import extractClique
 from cliqueProject import verifyClique
@@ -10,12 +10,12 @@ G = np.array(
 [0, 0, 1, 0, 0],
 [0, 1, 0, 0, 0]])
 
-x, c, errors = solve(G, renormL2, 1e-3, 1e-8, 10000, 100)
+x, c, errors = solve(G, renormL2, 1e-3, 0, 1e-8, 10000, 100)
 print c
 
 
 G = getGraph()
-y, R, errors2 = solve(G, renormCube, 1e-2, 1e-4, 100000000000, 1000)
+y, R, errors2 = solve(G, renormCube, 1e-2, 0.3, 1e-4, 100000000000, 1000)
 
 print R
 print len(R)
