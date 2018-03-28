4eb116e33fec0bad9e6a50ac789b3d8185856906
Artem Bolshakov <a.t.bolsh@gmail.com>
2018-03-28 14:39:44 -0400

    More or less done with the first stage. I implemented all norms and blocking. I'll add a 'global' experiment and finish for now.

---
+++ testingLn.py
@@ -0,0 +1,38 @@
+from NPLn import *
+from graphReader import getGraph
+from BigC import biggestClique
+from BigC import verifyClique
+
+#First, a simplistic test.
+G = np.array(
+[[0, 1, 1, 0, 0],
+[1, 0, 1, 0, 1],
+[1, 1, 0, 1, 0],
+[0, 0, 1, 0, 0],
+[0, 1, 0, 0, 0]])
+
+x, c, errors = solve(G, 8, renormL2, 1e-2, 1e-6, 1000000, 0)
+print c
+
+
+
+#Now, we load the full problem.
+G = getGraph()
+#We add the main diagonal (that's really it).
+H2 = graphToH(G)
+#We want the clique that has the most vertices, so we start the gradient descent with all of them
+x = np.ones(125)
+
+#Returns -- exact floating point numbers; only the ones that are part of the clique; and an error trace.
+#Takes: initial guess; H matrix; norm to be used; renormalization for x; 
+#stepsize; error cutoff; maxIter; how often to print.
+y, c2, errors2 = seek(x, H2, 4, renormL2, 1e-2, 1e-5, 100000000000000, 1000)
+
+
+#Gets the biggest k entries of y, under the condition that they form a clique.
+R = biggestClique(y, G)
+
+print R
+print len(R)
+print verifyClique(R, G)
+
